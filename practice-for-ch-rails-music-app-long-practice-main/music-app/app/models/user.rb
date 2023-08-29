5a007170fda682fe7b9bb7817e5fa747b5f2e477
Shannon Millar <shanenak@gmail.com>
2023-08-29 10:37:41 -0700

    Finish session token - generate, ensure, and reset

--- practice-for-ch-rails-music-app-long-practice-main/music-app/app/models/user.rb
+++ practice-for-ch-rails-music-app-long-practice-main/music-app/app/models/user.rb
@@ -1,3 +1,38 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+    validates :email, presence: true, uniqueness: true
+    validates :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+
+    private 
+
     
+
+    def generate_unique_session_token
+        session_token = SecureRandom.urlsafe_base64
+        while true
+            return session_token unless User.exists?(session_token: session_token)
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
 end
